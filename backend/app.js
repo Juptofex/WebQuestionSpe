ddce970a9badc9727fb697ae5d9b732dd5ea01e3
juptofex <julien.damilot@gmail.com>
2025-09-19 09:07:56 +0200

    feat: add expenses API with GET and POST endpoints, include CORS middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var expensesRouter = require('./routes/expenses');
 
 var app = express();
 
@@ -14,7 +15,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS middleware to allow requests from frontend
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', expensesRouter);
 
 module.exports = app;
